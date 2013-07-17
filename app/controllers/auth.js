ac7891f25f880a1068d394e36878ee2e8a66f86d
dlochrie <dlochrie@somethingelse.com>
2013-07-17 08:43:44 -0500

    Modified some var for User and Session.

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -4,16 +4,16 @@ exports.initialize = function(req, res) {
     locals = req.app.locals;
 
   if (session.user) {
-    req.app.locals.user = session.user;
+    locals.user = session.user;
     return;
   }
 
-  req.app.locals.user = false;
+  locals.user = false;
   session.user = false;
 
   if (session.passport.user) {
-    req.app.locals.user = session.passport.user;
-    
+    locals.user = session.passport.user;
+
     //c.User.find(session.passport.user, function (err, user) {
     /*
       if (!err || user) {
