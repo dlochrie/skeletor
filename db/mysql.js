10d93e25c2e993c2da7de9052baad702351dcf99
dlochrie <dlochrie@somethingelse.com>
2013-07-19 08:48:56 -0500

    Started to add DB configs, and then changed layouts and added greeking.

---
+++ db/mysql.js
@@ -0,0 +1,5 @@
+/**
+ * TODO: Please see this link for POOLING and ENV Vars for Uname/Password:
+ * http://stackoverflow.com/questions/16800418/how-to-properly-pass-mysql-connection-to-routes-with-express-js#answer-16800702
+ */
+var mysql      = require('mysql');
