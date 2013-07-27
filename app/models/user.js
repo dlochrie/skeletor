f94459aa1ca3f8881b89b84558c85a2eab81307c
dlochrie <dlochrie@somethingelse.com>
2013-07-27 16:38:05 -0500

    Added user model, views, and favicon.

---
+++ app/models/user.js
@@ -0,0 +1,18 @@
+var Model = require('./model');
+
+
+// Expose `User` Model
+module.exports = User;
+
+
+/**
+ * 
+ */
+function User(app, resource) {
+  this.app = app;
+  this.modelName = 'user';
+  Model.call(this, resource);
+}
+require('util').inherits(User, Model);
+
+// Validations should go here...
