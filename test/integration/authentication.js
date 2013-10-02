9991a3c54c8e4c41ab19fe8acec0a69f3435f545
dlochrie <dlochrie@somethingelse.com>
2013-10-01 20:20:33 -0500

    Added stubs for test setup and teardown, initial add for authentication integration test.

---
+++ test/integration/authentication.js
@@ -0,0 +1,62 @@
+var request = require('supertest'),
+  should = require('should'),
+  app = require('../../server');
+
+describe('Functional Test <Sessions>:', function () {
+  beforeEach(function(done) {
+    /**
+     * Create a mock session for a successfully logged-in user.
+     */
+    app.request.session = {
+      passport: {
+        user: {
+          user_displayName:  'Testing Tester'
+        }
+      },
+      logged_in: true
+    };
+    done();
+  });
+
+  it('should create user session for valid user', function (done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        var session = request.text;
+        res.text.should.include('Testing Tester');
+        res.text.should.not.include('login');
+        done();
+    });
+  });
+
+  // TODO: Move this to an ACL Test
+  it('should do something on the admin page', function (done) {
+    request(app)
+      .get('/admin')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        var session = request.text;
+        res.text.should.include('Administration Portals');
+        done();
+    });
+  });
+
+  it('should log the user out', function (done) {
+    request(app)
+      .get('/logout')
+      .end(function (err, res) {
+        if (err) return done(err);
+        request(app)
+          .get('/')
+          .end(function (err, res) {
+            if (err) return done(err);
+            res.text.should.not.include('Testing Tester');
+            res.text.should.include('login');
+            done();
+          });
+      });
+  });
+});
---
+++ test/setup.js
@@ -0,0 +1,5 @@
+// Build DB for tests.
+require('../db/mysql-install');
+
+// Seed DB for tests.
+// ...
